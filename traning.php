3d1121197f78f006d6b592ccbdb1567f6a9b2679
magesh <mageshsoft10@gmail.com>
2018-07-27 00:24:12 -0700

    test commit

---
+++ traning.php
@@ -0,0 +1,14 @@
+GIT
+
+top
+htop
+
+gerp -E 'pattern1|pattern2' filename
+
+
+Drives in Linux:
+1. bin(binaries)
+2. boot(boot information)
+3. home(user information)
+4. etc(configurations)
+5. dev(devices) 
